b591179e6fbe109d926eee426b96652e1f472c1b
brian-villa <brian.rj99@yahoo.com.br>
2024-10-16 19:16:35 +0100

    db connect and collection creation

--- dbConnect.py
+++ dbConnect.py
@@ -1,2 +1,22 @@
-from pymongo import MongoClient;
-from datetime 
+from pymongo import MongoClient
+from datetime import datetime
+
+#Connect to database (localhost ou instância remota)
+client = MongoClient("localhost", 27017)
+db = client["ProjetoADMRedesBD"] #db name
+collection = db["trafego_rede"]
+
+#function to save mongodb packages
+
+def save_package_mongo(ip_origem, ip_destino, protocolo, tamanho_pacote) : pacote {
+    'ip_origem': ip_origem,
+    'ip_destino': ip_destino,
+    'protocolo': protocolo,
+    'tamanho_pacote': tamanho_pacote,
+    'timestamp': datetime.now()
+}
+try:
+    collection.insert_one(pacote)
+    print("Pacote salvo no Mongodb")
+except Exception as e:
+    print(f"Erro ao salvar no MongoDB: {e}")
